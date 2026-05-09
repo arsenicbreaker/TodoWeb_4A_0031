f63a80809db771243c8fe53c0a420b5101397d5d
Agustia Eriani <agustiaeriani@gmail.com>
2026-05-09 18:26:13 +0700

    add task date and status toggle

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const inputTugas = document.getElementById("inputTugas");
 const btntambah = document.getElementById("btnTambah");
 const daftarTugas = document.getElementById("daftarTugas");
+const tanggal = document.getElementById("inputTanggal");
 
 btntambah.addEventListener("click", function () {
   let teksTugas = inputTugas.value;
+  let tanggalTugas = tanggal.value;
 
   if (teksTugas === "") {
     alert("Data harus dimasukkan!");
@@ -12,9 +14,27 @@ btntambah.addEventListener("click", function () {
 
   let listbaru = document.createElement("li");
   let spanbaru = document.createElement("span");
+  let btnStatus = document.createElement("button");
+  let datebaru = document.createElement("date");
 
   spanbaru.innerHTML = teksTugas;
+  datebaru.innerHTML = tanggalTugas;
   listbaru.appendChild(spanbaru);
+  listbaru.appendChild(datebaru);
+  daftarTugas.appendChild(listbaru);
+  btnStatus.innerHTML = "progress";
+
+  btnStatus.addEventListener("click", function () {
+    if (btnStatus.innerHTML === "progress") {
+      btnStatus.innerHTML = "done";
+    } else {
+      btnStatus.innerHTML = "progress";
+    }
+  });
+
+  listbaru.appendChild(spanbaru);
+  listbaru.appendChild(datebaru);
+  listbaru.appendChild(btnStatus);
   daftarTugas.appendChild(listbaru);
 
   const warnabaru = document.querySelectorAll("li");
